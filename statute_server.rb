c634b5c170b1d16c0d0f05a6c417db4431d04053
Ryan Smith <zeepickle@gmail.com>
2015-12-19 11:15:09 -0800

    logic completed for requirements and in progress for compliance

--- statute_server.rb
+++ statute_server.rb
@@ -37,17 +37,43 @@ get '/' do
   if retrieve.include?(@routes[0])
     statute_text = @statutes[data["statute"]][@routes[0]]
     if statute_text.nil?
-      return return_error_msg("The text for this statue was not found.")
+      return return_error_msg("The #{@routes[0]} for this statue was not found.")
     else
       payload[@routes[0]] = statute_text
     end
   end
-  if retrieve.include?("requirements")
+  if retrieve.include?(@routes[1])
+    statute_requirements = @statutes[data["statute"]][@routes[1]]
+    if statute_requirements.nil?
+      return return_error_msg("The #{@routes[1]} for this statute were not found.")
+    else
+      payload[@routes[1]] = statute_requirements
+    end
   end
-  if retrieve.include?()
+  if retrieve.include?(@routes[2])
+    observed_data = data["observed_data"]
+    if observed_data.nil?
+      return return_error_msg("The observed_data was missing and is required to determine #{@routes[2]}.")
+    end
+    statute_requirements = @statutes[data["statute"]][@routes[1]]
+    if statute_requirements.nil?
+      return return_error_msg("The #{@routes[1]} for this statute to determine #{@routes[2]} was missing.")
+    end
+    payload[@routes[2]] = determine_compliance(statute_requirements, observed_data)
+  end
+  return return_data(payload)
 end
 
 def return_error_msg(msg)
   content_type :json
   {error: msg}.to_json
+end
+
+def return_data(data)
+  content_type :json
+  data.to_json
+end
+
+def determine_compliance(requirements, data)
+
 end
